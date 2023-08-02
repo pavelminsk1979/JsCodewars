e46d3ef0f27d781534342ee252c563d154216a4d
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-08-02 09:39:01 +0300

    task 26

--- mainForStudents.js
+++ mainForStudents.js
@@ -2,10 +2,16 @@
 
 
 
+const flip=(d, a)=>{
+if(d==='R'){
+    return a.sort((a,b)=>a-b)
+}  if(d==='L'){return a.sort((a,b)=>a-b).reverse()}
+
+}
 
 
-
-
+console.log(flip('R', [3, 2, 1, 2]))
+console.log(flip('L', [1, 4, 5, 3, 5]))
 
 
 
