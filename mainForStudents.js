53aaeb589cf0d5922a882a62f1b39338d1a25e83
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-08-21 14:41:57 +0300

    task 6

--- mainForStudents.js
+++ mainForStudents.js
@@ -1,5 +1,65 @@
 
 
+/*
+
+Понедельник --> 12
+
+Вторник --> числа больше 95
+
+Среда --> 34
+
+Четверг --> 0
+
+Пятница --> числа, делящиеся на 2
+
+Суббота --> 56
+
+Воскресенье --> 666 или -666
+*/
+
+/*
+var AmIAfraid = function(day, num){
+     if(day==='Monday'&&num===12){return true}
+    else if(day==='Tuesday'&&num>95){return true}
+    else if(day==='Wednesday'&&num===34){return true}
+    else if(day==='Thursday'&&num===0){return true}
+    else if(day==='Friday'&&num%2===0){return true}
+    else if(day==='Saturday'&&num===56){return true}
+    else if(day==='Sunday'&&(num===666||num===-666)){return true}
+    else {return false}
+
+}
+console.log(AmIAfraid("Monday", 13))
+console.log(AmIAfraid("Sunday", -666))
+console.log(AmIAfraid("Tuesday", 2))
+console.log(AmIAfraid("Tuesday", 965))
+console.log(AmIAfraid("Friday", 2))*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*Вам будет предоставлен список из двух строк, каждая из которых будет содержать ровно одно двоеточие (":") в середине (но не в начале или в конце). Длина строк до и после двоеточия является случайной.
 
     Ваша задача состоит в том, чтобы вернуть список из двух строк (в том же порядке, что и исходный список), но с заменой символов после каждого двоеточия.*/
