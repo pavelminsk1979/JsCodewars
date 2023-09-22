a1a4a57e17349c44f8043c9aebdc93e03753bfe3
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-09-22 10:18:23 +0300

    task 28

--- mainForStudents.js
+++ mainForStudents.js
@@ -15,6 +15,77 @@
 
 
 
+
+
+
+
+
+
+
+
+
+/*
+
+function battle(x, y) {
+    let data = {
+        A:1,
+        B:2,
+        C:3,
+        D:4,
+        E:5,
+        F:6,
+        G:7,
+        H:8,
+        I:9,
+        J:10,
+        K:11,
+        L:12,
+        M:13,
+        N:14,
+        O:15,
+        P:16,
+        Q:17,
+        R:18,
+        S:19,
+        T:20,
+        U:21,
+        V:22,
+        W:23,
+        X:24,
+        Y:25,
+        Z:26
+    }
+    let arr= x.split('')
+    let numX=0
+    for (let i=0;i<arr.length;i++){
+        numX=numX+data[arr[i]]
+    }
+    let arrY= y.split('')
+    let numY=0
+    for (let i=0;i<arrY.length;i++){
+        numY=numY+data[arrY[i]]
+    }
+    if(numX===numY){return "Tie!"}
+    if(numX>numY){return x}
+    if(numX<numY){return y}
+}
+
+
+console.log(battle("AAA", "Z"))
+console.log(battle("ONE", "TWO"))
+console.log(battle("ONE", "NEO"))
+*/
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 
