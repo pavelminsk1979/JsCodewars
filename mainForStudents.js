2dc180b34cc547e53018358dfe4034b32be8210e
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-10-04 23:47:57 +0300

    task 36

--- mainForStudents.js
+++ mainForStudents.js
@@ -1,6 +1,52 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+function isLeapYear(year) {
+   if(year%400===0){return true}
+   else if (year%100===0){return false}
+   else if(year%4===0){return true}else {return false}
+}
+
+console.log(isLeapYear(2020))
+console.log(isLeapYear(2000))
+console.log(isLeapYear(2015))
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 function getMean(arr, x, y) {
     if(x<2){return -1}
