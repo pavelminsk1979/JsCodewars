171fc267f5370fdfa7223ccf35d670c61d63c85c
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-10-16 13:13:38 +0300

    task 42

--- mainForStudents.js
+++ mainForStudents.js
@@ -19,6 +19,34 @@
 
 
 
+/*function openOrSenior(data){
+    let res = []
+   for(let i=0;i<data.length;i++){
+      if(  data[i][0]>=55&&data[i][1]>7){
+          res.push('Senior')
+      }else {res.push('Open')}
+   }
+   return res
+}
+
+console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
