cea9e04f42fd652b181d208b2118e30c38da10bf
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-19 17:20:06 +0300

    task 81

--- mainForStudents.js
+++ mainForStudents.js
@@ -3,9 +3,27 @@
 
 
 
+function quicksum(packet){
+    let ob = {A:1, B:2, C:3, D:4, E:5, F:6, G:7, H:8, I:9, J:10,K:11, L:12, M:13, N:14, O:15, P:16, Q:17, R:18, S:19, T:20, U:21, V:22, W:23, X:24, Y:25, Z:26}
+ let a = packet.split('')
+    let res=[]
+    for (let i=0;i<a.length;i++){
+if(a[i]===' '){res.push(0)}else {
+    res.push(ob[a[i]]*(i+1))
+}
 
+    }
 
+    if(res[0]===0||res[res.length-1]===0){return 0} else  {
 
+        let nn=  res.reduce((ac,el)=>ac+el)
+        if( isNaN(nn)){return 0} else {return nn}
+    }
+
+}
+
+console.log(quicksum("A C M"))
+console.log(quicksum("MID CENTRAL"))
 
 
 
