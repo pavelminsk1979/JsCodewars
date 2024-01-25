2fa3b383e592744a570112b3d07d542e1a538232
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-01-25 22:49:52 +0300

    task 96.

--- mainForStudents.js
+++ mainForStudents.js
@@ -29,6 +29,44 @@
 
 
 
+
+/*
+
+<script>
+    function removeWhitespaces(string) {
+    return string.split(' ').filter(s => s !== '').join('_')
+}
+
+    console.log(removeWhitespaces('you are JS developer'))
+</script>
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 
