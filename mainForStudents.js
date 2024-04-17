9a87c7a303b86c807a78be90558bdd498521235b
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-04-17 19:22:55 +0300

    task 129

--- mainForStudents.js
+++ mainForStudents.js
@@ -19,6 +19,51 @@
 
 
 
+
+
+
+
+/*
+
+function cypher(string) {
+    let listLeters={
+        I:1,
+        R:2,
+        E:3,
+        A:4,
+        S:5,
+        G:6,
+        T:7,
+        B:8,
+        O:'0',
+        l:1,
+        z:2,
+        e:3,
+        a:4,
+        s:5,
+        b:6,
+        t:7,
+        g:9,
+        o:'0'}
+   const arr = string.split('')
+    let res = []
+    for(let i=0;i<arr.length;i++){
+
+        let leter = arr[i]
+        if(listLeters[leter]){
+            res.push(listLeters[leter])
+        }else {res.push(leter)}
+    }
+
+    return res.join('')
+}
+
+console.log(cypher("Hello World"))
+
+
+*/
+
+
 /*
 
 function encode(str,  n) {
