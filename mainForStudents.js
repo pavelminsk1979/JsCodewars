75d27fcbe63a0a5bf26232f28a91839208cde9f6
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-07-11 03:52:42 +0300

    task 4

--- mainForStudents.js
+++ mainForStudents.js
@@ -23,8 +23,50 @@
 
 
 
+/*function decrypt(encryption) {
+    const arr = [...encryption]
+    let res = {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: 0,
+        g: 0,
+        h: 0,
+        i: 0,
+        j: 0,
+        k: 0,
+        l: 0,
+        m: 0,
+        n: 0,
+        o: 0,
+        p: 0,
+        q: 0,
+        r: 0,
+        s: 0,
+        t: 0,
+        u: 0,
+        v: 0,
+        w: 0,
+        x: 0,
+        y: 0,
+        z: 0
+    }
+    for (let i = 0; i < arr.length; i++) {
+        const big = arr[i].toUpperCase()
+        const smile = arr[i].toLowerCase()
+        if (big === smile) continue
+        if (smile===arr[i]){
+            res[smile] += 1
+        }
+
+    }
+    return Object.values(res).join('')
+}
 
 
+console.log(decrypt('$aaaa#bbb*ccfff!z'))*/
 
 
 /*function solve(s){
@@ -53,21 +95,6 @@
 
 console.log(solve('abbba'))
 console.log(solve('aabbccddd'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*function beggars(values, n) {
